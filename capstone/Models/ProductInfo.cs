da55ccd717704346ab6b9a4a9a22084192512a8f
jeo hyun <jack8121313@gmail.com>
2023-09-20 19:34:48 +0900

    로고 이미지 작업 + 상품 db연동

---
+++ capstone/Models/ProductInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace capstone.Models
+{
+    public class ProductInfo
+    {
+        [Key]
+        public int productNum { get; set; }
+        public int productcategory { get; set; }
+            
+        public string name { get; set; }
+            
+        public int manufacturer{ get; set; }
+            
+        public int price { get; set; }
+            
+        public bool bundledeliveryfee{ get; set; }
+            
+        public bool onSale { get; set; }
+        public bool soldOut { get; set; }
+
+        public string image { get; set; }
+
+        public string descriptionHtml { get; set; }
+        
+    }
+}
