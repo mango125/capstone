4b6837f2852807d75ba29d008451340b67147ad9
jeo hyun <jack8121313@gmail.com>
2023-11-08 19:45:56 +0900

    ProductController Mainboard Select Action

--- capstone/Controllers/ProductController.cs
+++ capstone/Controllers/ProductController.cs
@@ -8,6 +8,7 @@ using Microsoft.Identity.Client;
 using System;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using Microsoft.AspNetCore.Components;
+using Azure.Core;
 
 namespace capstone.Controllers
 {
@@ -27,19 +28,72 @@ namespace capstone.Controllers
             var categoryId = request.Query["category"];
             FormattableString query;
 
-            if (categoryId[0] == "mainboard") 
+            string whereQuery = "";
+            string SortBy;
+
+            //공통select로 들어갈 구문
+            //제조사
+            if (request.Query["manufacturer"].Any())
             {
-                var Selectedmanufacturer = request.Query["manufacturer"].ToArray();
-                if(Selectedmanufacturer.Any())
+                string[] Selectedmanufacturer;
+                Selectedmanufacturer = request.Query["manufacturer"].ToArray();
+                foreach(string manufacturer in Selectedmanufacturer)
                 {
-                    query = $"select * from View_product_mainboard where productManufacturer = {Selectedmanufacturer}";
+                    whereQuery += " productManufacturer = '" + manufacturer + "' OR ";
+                }
+            }
+            //정렬
+            if (request.Query["sort"] == "REV")
+            {
+                SortBy = "ORDER BY REVIEW_COUNTER DESC";
+            }
+            else
+            {
+                SortBy = "ORDER BY productPrice " + request.Query["sort"].ToString();
+            }
+
+            //카테고리가 메인보드일 경우
+            if (categoryId == "mainboard") 
+            {
+                //태그입력 시 실행               
+                whereQuery += CheckTags(request, "mainboardCpuSocket", "mainboardCpuSocket"); //CPU소켓
+                whereQuery += CheckTags(request, "mainboardFormfactor", "mainboardFormfactor"); //폼펙터
+                whereQuery += CheckTags(request, "mainboardRamSocket", "mainboardRamSocket"); //렘 소켓
+                //메모리 슬롯 수
+                string memorySlots = "";
+                if (request.Query["mainboardMemorySlots"].Any())
+                {
+                    memorySlots = "mainboardMemorySlots = " + request.Query["mainboardMemorySlots"];
+                }
+                //조건식 만약 whereQuery에 값이 있을 때 앞에 where을 추가
+                //또한 memorySlots에 값이 있을 때 whereQuery 앞 뒤에 괄호와 AND 추가
+                if (whereQuery.Any())
+                {
+                    //맨 뒤의 OR을 제거
+                    whereQuery = whereQuery.Substring(0, whereQuery.Length - 3);
+                    //메모리 슬롯에 값이 있을 경우
+                    if (memorySlots.Any())
+                    {
+                        whereQuery = " WHERE (" + whereQuery + ") AND" + memorySlots;
+                    }
+                    else
+                    {
+                        whereQuery = " WHERE " + whereQuery;
+                    }
+                    //formattablestring 형태의 변수 사용 시 where구문을 제대로 인식하지 못하여 string형태의 변수를 이용
+                    string strQuery = $@"select * from View_product_mainboard {whereQuery}";
+
+                    Product_Mainboard[] product_Mainboards = _db.Product_Mainboard.FromSqlRaw(strQuery).ToArray();
+                    ViewBag.productInfo = product_Mainboards;
+                    return View();
+
                 }
                 else
                 {
                     query = $"select * from View_product_mainboard";
+                    Product_Mainboard[] product_Mainboards = _db.Product_Mainboard.FromSql(query).ToArray();
+                    ViewBag.productInfo = product_Mainboards;
                 }
-                Product_Mainboard[] product_Mainboards = _db.Product_Mainboard.FromSql(query).ToArray();
-                ViewBag.productInfo = product_Mainboards;
 
             }
             else if (categoryId == "CPU")
@@ -90,9 +144,26 @@ namespace capstone.Controllers
                 ViewBag.productInfo = "HDD";
                 //    return View();
             }
+
             return View();
         }
 
+        //태그값을 확인하는 메서드
+        //tag = query문에 직접 입력될 속성 명
+        //value = request에서 속성을 찾을 때 사용될 속성 명
+        public string CheckTags(HttpRequest request, string tag, string value)
+        {
+            string whereQuery = "";
+            string[] SelectedQuery = request.Query[value];
+            if (SelectedQuery.Any())
+            {
+                foreach (string selectedValue in SelectedQuery)
+                {
+                    whereQuery += tag + " = '" + selectedValue + "' OR ";
+                }
+            }
+            return whereQuery;
+        }
 
         [HttpGet]
         public IActionResult ProductInfo(string category, int productNum)
